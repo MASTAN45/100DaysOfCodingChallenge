8f49c3af1d2609c2df6ae8fa40d495037f46d538
SHAIK MASTAN VALI <142141981+MASTAN45@users.noreply.github.com>
2025-07-29 02:00:24 +0530

    Create Day27.java

---
+++ Day27.java
@@ -0,0 +1,41 @@
+383. Ransom Note
+  class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap <Character, Integer> map= new HashMap<>();
+
+        for(char c : magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+
+        for(char c: ransomNote.toCharArray()){
+            if(!map.containsKey(c) || map.get(c)==0){
+                return false;
+            }
+
+            map.put(c,map.get(c)-1);
+        }
+        return true;
+
+        
+    }
+}
+
+242. Valid Anagram
+  class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(t.length() !=s.length()) return false;
+        HashMap<Character, Integer> map =new HashMap<>();
+
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) +1);
+        }
+
+        for(char c:t.toCharArray()){
+            if(!map.containsKey(c) || map.get(c)==0){
+                return false;
+            }
+            map.put(c, map.get(c)-1);
+        }
+        return true;
+    }
+}
